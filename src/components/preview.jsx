f4d19b31b45162233572517b805e5645e3f19f7b
silentgraphic <kierannolan1995@gmail.com>
2024-09-25 13:25:17 +0100

    Added syntax highlighting

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -1,7 +1,15 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css';
 
 function Preview() {
     let test = useSelector(state => state.markdown.markdown);
+
+    useEffect(() => {
+        const nodes = document.querySelectorAll('code');
+        nodes.forEach(node => hljs.highlightBlock(node));
+    });
 
     return (
         <div id="preview" dangerouslySetInnerHTML={{ __html: test }}></div >
